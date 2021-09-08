3fecbf86e1952cf3b6361ccae7a8e7be132c6232
Prateek SU <pankajsunal123@gmail.com>
2021-09-08 16:31:46 +0530

    Fix dark mode

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -47,7 +47,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> with AutomaticK
         SwitchListTile(
           title: const Text('Dark mode'),
           value: context.isDark,
-          onChanged: (bool value) => ref.read(themeTypeProvider.notifier).themeType = value ? 0 : 1,
+          onChanged: (bool value) => ref.read(themeTypeProvider.notifier).themeType = value ? 2 : 1,
         ),
       ],
     );
