46d1aecd6270460808be38cb7021925427648090
Prateek SU <pankajsunal123@gmail.com>
2022-01-07 16:54:14 +0530

    Bump version to 1.1.0

--- lib/utils/shared_prefs.dart
+++ lib/utils/shared_prefs.dart
@@ -1,13 +1,13 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MyPrefs {
-  static final MyPrefs _instance = MyPrefs._ctor();
+  static final MyPrefs _instance = MyPrefs._internal();
 
   factory MyPrefs() {
     return _instance;
   }
 
-  MyPrefs._ctor();
+  MyPrefs._internal();
 
   late SharedPreferences _prefs;
 
