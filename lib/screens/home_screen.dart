b79d7a840fb5448b45f72a49b17b2a4ba0c43429
Prateek SU <pankajsunal123@gmail.com>
2022-02-08 12:44:07 +0530

    Add trending section at home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:flutube/utils/utils.dart';
 import 'package:flutube/widgets/widgets.dart';
+import 'package:piped_api/piped_api.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,23 +18,37 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   Widget build(BuildContext context) {
     super.build(context);
+
+    final videos =
+        PipedApi().getUnauthenticatedApi().trending(region: Regions.IN);
+    print(videos);
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 12.0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Container(
-            constraints: const BoxConstraints(maxWidth: 6 * 300),
-            child: MasonryGridView.count(
-              crossAxisCount: (context.width ~/ 300 as int).clamp(1, 6),
-              shrinkWrap: true,
-              primary: false,
-              itemBuilder: (ctx, idx) => const FTVideo(
-                videoUrl: 'https://www.youtube.com/watch?v=WhWc3b3KhnY',
-              ),
-              itemCount: 4,
-            ),
-          ),
+          FutureBuilder<Response>(
+              future: videos,
+              builder: (context, snapshot) {
+                print(snapshot.data?.data);
+                if (snapshot.hasData) {
+                  return Container(
+                    constraints: const BoxConstraints(maxWidth: 6 * 300),
+                    child: MasonryGridView.count(
+                      crossAxisCount: (context.width ~/ 300 as int).clamp(1, 6),
+                      shrinkWrap: true,
+                      primary: false,
+                      itemBuilder: (ctx, idx) => FTVideo(
+                        videoUrl: "https://youtube.com" +
+                            (snapshot.data!.data[idx] as StreamItem).url,
+                      ),
+                      itemCount: snapshot.data!.data.length,
+                    ),
+                  );
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              }),
         ],
       ),
     );
