1e7a11a195aaccca6c9a331a0de3233cdcb0ae65
Prateek SU <prtksu@pm.me>
2022-07-09 12:21:04 +0530

    Try to add video player

--- lib/ui/widgets/video_player.dart
+++ lib/ui/widgets/video_player.dart
@@ -1,6 +1,4 @@
-import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
-import 'package:pstube/data/extensions/extensions.dart';
 
 class VideoPlayer extends StatefulWidget {
   const VideoPlayer({
@@ -17,72 +15,24 @@ class VideoPlayer extends StatefulWidget {
 }
 
 class _VideoPlayerState extends State<VideoPlayer> with WidgetsBindingObserver {
-  late BetterPlayerController _controller;
-
   @override
   void initState() {
     super.initState();
 
     WidgetsBinding.instance.addObserver(this);
-
-    mediaPlayerControllerSetUp();
-    _controller.setOverriddenFit(BoxFit.contain);
-  }
-
-  void mediaPlayerControllerSetUp() {
-    _controller = BetterPlayerController(
-      BetterPlayerConfiguration(
-        autoDetectFullscreenAspectRatio: true,
-        fit: BoxFit.fitHeight,
-
-        aspectRatio: 16 / 9,
-        handleLifecycle: false,
-        autoDetectFullscreenDeviceOrientation: true,
-        // autoPlay: true,
-        allowedScreenSleep: false,
-        // autoDispose: true,
-        controlsConfiguration: BetterPlayerControlsConfiguration(
-          overflowModalColor: context.getBackgroundColor.withOpacity(0.9),
-          overflowMenuIconsColor: context.textTheme.bodyText1!.color!,
-          overflowModalTextColor: context.textTheme.bodyText1!.color!,
-          playIcon: Icons.play_arrow,
-          playerTheme: BetterPlayerTheme.material,
-          loadingWidget: const CircularProgressIndicator(),
-          progressBarPlayedColor: context.theme.primaryColor.withOpacity(0.92),
-          progressBarBufferedColor: Colors.grey,
-          progressBarHandleColor: context.theme.primaryColor,
-        ),
-      ),
-      betterPlayerDataSource: BetterPlayerDataSource(
-        BetterPlayerDataSourceType.network,
-        widget.url,
-        bufferingConfiguration: const BetterPlayerBufferingConfiguration(
-          minBufferMs: 60000,
-          maxBufferMs: 555000,
-        ),
-        resolutions: widget.resolutions,
-        cacheConfiguration: const BetterPlayerCacheConfiguration(
-          useCache: true,
-          preCacheSize: 400000,
-          maxCacheSize: 400000,
-          maxCacheFileSize: 400000,
-        ),
-      ),
-    );
   }
 
   @override
   void dispose() {
     super.dispose();
     WidgetsBinding.instance.removeObserver(this);
-
-    _controller
-      ..clearCache()
-      ..dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BetterPlayer(controller: _controller);
+    return VideoPlayer(
+      url: widget.url,
+      resolutions: widget.resolutions,
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import bitsdojo_window_macos
 import connectivity_plus_macos
-import dart_vlc
 import package_info_plus_macos
 import path_provider_macos
 import screen_retriever
@@ -15,13 +14,11 @@ import share_plus_macos
 import shared_preferences_macos
 import sqflite
 import url_launcher_macos
-import wakelock_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  DartVlcPlugin.register(with: registry.registrar(forPlugin: "DartVlcPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
@@ -29,6 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
