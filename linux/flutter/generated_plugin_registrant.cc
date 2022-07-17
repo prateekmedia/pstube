069d8087e5868a77f6ddde3cf19f2fa95f629ef9
Prateek SU <prtksu@pm.me>
2022-07-17 21:10:38 +0530

    Add Windows exe

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -8,6 +8,7 @@
 
 #include <bitsdojo_window_linux/bitsdojo_window_plugin.h>
 #include <dart_vlc/dart_vlc_plugin.h>
+#include <handy_window/handy_window_plugin.h>
 #include <screen_retriever/screen_retriever_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 #include <window_manager/window_manager_plugin.h>
@@ -19,6 +20,9 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) dart_vlc_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "DartVlcPlugin");
   dart_vlc_plugin_register_with_registrar(dart_vlc_registrar);
+  g_autoptr(FlPluginRegistrar) handy_window_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "HandyWindowPlugin");
+  handy_window_plugin_register_with_registrar(handy_window_registrar);
   g_autoptr(FlPluginRegistrar) screen_retriever_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverPlugin");
   screen_retriever_plugin_register_with_registrar(screen_retriever_registrar);
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   bitsdojo_window_linux
   dart_vlc
+  handy_window
   screen_retriever
   url_launcher_linux
   window_manager
