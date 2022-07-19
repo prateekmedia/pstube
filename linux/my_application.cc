0da299328c9602ba0eb7fe585c14eb14e5ce4f73
Prateek SU <prtksu@pm.me>
2022-07-19 20:05:06 +0530

    Fix linux build

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -50,17 +50,21 @@ static void my_application_activate(GApplication *application) {
 
   auto bdw = bitsdojo_window_from(window);
   bdw->setCustomFrame(true);
+  gtk_window_set_default_size(window, 1280, 720);
+  gtk_widget_show(GTK_WIDGET(window));
+
+  auto gdk_window = gtk_widget_get_window(GTK_WIDGET(window));
+  gdk_window_set_decorations(gdk_window, GdkWMDecoration::GDK_DECOR_BORDER);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
   FlView *view = fl_view_new(project);
+  gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
 
-  gtk_widget_show(GTK_WIDGET(window));
-  gtk_widget_show(GTK_WIDGET(view));
   gtk_widget_grab_focus(GTK_WIDGET(view));
 }
 
